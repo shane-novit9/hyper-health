3e34c92ad6bbbf4f5ac51ad9f6ecd5d0059a4e5d
shane-novit9 <novitsky.shane9@gmail.com>
2023-08-18 17:32:20 -0400

    Clean up

--- SQL/create-tables.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS account CASCADE;
-
-CREATE TABLE account (
-    email varchar(35),
-    password char(60),
-    rating float(2,1),
-    active boolean default true
-    PRIMARY KEY (`email`)
-);
